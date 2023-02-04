0b3aba24b61ef81a867abe217bad560fda3c7efb
Gilmar <geoh222@unesc.net>
2023-02-03 23:27:41 -0300

    Geração e Compartilhamento
    -Refeita parte de gerar mensagem via JavaScript
    -Adição dos botões de compartilhamento (Não Funcionais)

--- dados.js
+++ dados.js
@@ -6,12 +6,35 @@ let username;
         adj = document.getElementById("adjInput").value;
         msg = document.getElementById("msgInput").value;
 
-        document.getElementById("myLabel").innerHTML = "Feliz aniversário "+username; 
-        document.getElementById("myLabel 2").innerHTML = " que você possa cair mais vezes de bike na minha frente e que você abra assas de mundo do " +cidade;
-        document.getElementById("myLabel 3").innerHTML = " para o mundo real para cair mais vezes na minha frente seu " +adj;
-        document.getElementById("myLabel 4").innerHTML = "feliz aniversário "+msg;
-
+        var fraseFinal = "Feliz aniversário " +username + " que você possa cair mais vezes de bike na minha frente e que você abra assas de mundo do " +cidade + " para o mundo real para cair mais vezes na minha frente seu " +adj + "feliz aniversário "+msg;
+        document.getElementById("resultado").innerHTML = fraseFinal;
     }
+
+    document.getElementById("btnCompartilhar").addEventListener("click", function() {
+        // código para compartilhar a mensagem no WhatsApp
+      });
+
+      document.getElementById("btnCompartilhar").addEventListener("click", function() {
+        var mensagem = encodeURIComponent(fraseFinal);
+        window.open("https://api.whatsapp.com/send?text=" + mensagem);
+      });
+
+
+    document.getElementById("btnCopiar").addEventListener("click", function() {
+        // código para copiar o texto para a área de transferência
+    });
+
+    document.getElementById("btnCopiar").addEventListener("click", function() {
+        navigator.clipboard.writeText(fraseFinal).then(function() {
+          console.log("Texto copiado com sucesso para a área de transferência.");
+        }, function(err) {
+          console.error("Erro ao copiar o texto para a área de transferência:", err);
+        });
+      });
+      
+      
+
+
         //Feliz aniversário (NOME) que vc possa cair mais vezes de bike na minha frente e que vc abra assas do mundo do 
         //(CIDADE) para o mundo real para cair mais vezes na minha frente seu ladrao caixa dois de 
         //churrasco dos guri pai feliz aniversário (ALGUÉM ALEATÓRIO)
