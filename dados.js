adec1cfa618a52c644d7ed6767b05fad7f4861c1
Gilmar <geoh222@unesc.net>
2023-02-04 00:55:38 -0300

    Compartilhamento
    Botões de compartilhamento integrados

--- dados.js
+++ dados.js
@@ -1,34 +1,41 @@
-let username;
+    document.addEventListener("DOMContentLoaded", function() {
 
-    document.getElementById("btnGerar").onclick = function(){
-        username = document.getElementById("nomeInput").value;
-        cidade = document.getElementById("cidadeInput").value;
-        adj = document.getElementById("adjInput").value;
-        msg = document.getElementById("msgInput").value;
-
-        var fraseFinal = "Feliz aniversário " +username + " que você possa cair mais vezes de bike na minha frente e que você abra assas de mundo do " +cidade + " para o mundo real para cair mais vezes na minha frente seu " +adj + "feliz aniversário "+msg;
-        document.getElementById("resultado").innerHTML = fraseFinal;
-    }
-
-    document.getElementById("btnCompartilhar").addEventListener("click", function() {
-        // código para compartilhar a mensagem no WhatsApp
-      });
-
-      document.getElementById("btnCompartilhar").addEventListener("click", function() {
-        var mensagem = encodeURIComponent(fraseFinal);
-        window.open("https://api.whatsapp.com/send?text=" + mensagem);
-      });
-
-    document.getElementById("btnCopiar").addEventListener("click", function() {
-        // código para copiar o texto para a área de transferência
-    });
-
-    document.getElementById("btnCopiar").addEventListener("click", function() {
-        navigator.clipboard.writeText(fraseFinal).then(function() {
-          console.log("Texto copiado com sucesso para a área de transferência.");
-        }, function(err) {
-          console.error("Erro ao copiar o texto para a área de transferência:", err);
-        });
+        document.getElementById("btnGerar").onclick = function(){
+            username = document.getElementById("nomeInput").value;
+            cidade = document.getElementById("cidadeInput").value;
+            adj = document.getElementById("adjInput").value;
+            msg = document.getElementById("msgInput").value;
+    
+            var fraseFinal = "Feliz aniversário " +username + " que você possa cair mais vezes de bike na minha frente e que você abra assas de mundo do " +cidade + " para o mundo real para cair mais vezes na minha frente seu " +adj + " feliz aniversário "+msg;
+            document.getElementById("resultado").innerHTML = fraseFinal;
+        }
+        var btnCompartilhar = document.getElementById("btnCompartilhar");
+      
+        if (btnCompartilhar) {
+          btnCompartilhar.addEventListener("click", function() {
+            var textoCompartilhar = encodeURIComponent(resultado.textContent);
+            var urlCompartilhar = "https://api.whatsapp.com/send?text=" + textoCompartilhar;
+            window.open(urlCompartilhar);
+          });
+        } else {
+          console.error("Elemento com ID 'btnCompartilhar' não encontrado na página");
+        }
+        var btnCopiar = document.getElementById("btnCopiar");
+        if (btnCopiar) {
+          btnCopiar.addEventListener("click", function() {
+            if (resultado) {
+              navigator.clipboard.writeText(resultado.textContent).then(function() {
+                console.log("Texto copiado com sucesso: " + resultado);
+              }, function(error) {
+                console.error("Erro ao copiar o texto: " + error);
+              });
+            } else {
+              console.error("A variável 'fraseFinal' não está definida ou está vazia");
+            }
+          });
+        } else {
+          console.error("Elemento com ID 'btnCopiar' não encontrado na página");
+        }
       });
     
       
